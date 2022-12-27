6ab5555a5b891b1d03a0af55d964fcd820c0bbea
Kuba Trzciński <kubatk@interia.eu>
2022-12-27 12:30:11 +0100

    first draft of database structure

---
+++ database/migrations/2022_12_12_131951_update_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->string('last_name');
+            $table->string('address');
+            $table->integer('class');
+            $table->char('group');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('last_name');
+            $table->dropColumn('address');
+            $table->dropColumn('class');
+        });
+    }
+};
---
+++ database/migrations/2022_12_12_134522_create_marks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->char('sign');
+            $table->string('name');
+            $table->float('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+};
