44978e4584790a82b8cf405780e3356233e47729
Kuba Trzciński <kubatk@interia.eu>
2023-01-02 15:00:13 +0100

    basic form test

---
+++ resources/views/headmaster/add_teacher.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.headmaster')
+
+@section('content')
+<h3>
+    Dodawanie nauczyciela
+</h3>
+<form>
+    Imie: <input type="text" /><br>
+    Nazwisko: <input type="text" /><br>
+    Adres: <input type="text" /><br>
+</form>
+@endsection
