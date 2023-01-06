b6be1b91d4158caecafb5383b7a95616242f6638
Kuba Trzciński <kubatk@interia.eu>
2023-01-06 11:59:59 +0100

    listing users
    /manage-users

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 // Headmaster
 Route::get('/add-user', function (){return view('headmaster.add_user');})->name('add_user');
 Route::post('addUser', [App\Http\Controllers\HeadmasterController::class, 'addUser']);
+
+Route::get('/manage-users', function (){return view('headmaster.manage_users');})->name('manage_users');
 // Teacher
 
 // Student
